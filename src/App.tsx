0657288ca265ff0b6892bd417397ca22cc2a06c7
Arya <ajaarya@apac.corpdir.net>
2022-08-23 01:44:24 +0530

    testing deployment

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload successfuly.
+          Edit <code>src/App.tsx</code> and save to reload successfulyy.
         </p>
         <a
           className="App-link"
