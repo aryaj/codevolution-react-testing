de70af7c032c538a96480e5bde41c2adf12e2239
Arya <ajaarya@apac.corpdir.net>
2022-08-23 01:25:36 +0530

    hosted

--- src/components/Greet.test.tsx
+++ src/components/Greet.test.tsx
@@ -7,19 +7,22 @@ test('Greet renders correctly',()=>{
     expect(textElement).toBeInTheDocument();
 }) */
 
+import { render, screen } from "@testing-library/react";
+import { Greet } from "./Greet";
 
-import { render, screen } from "@testing-library/react"
-import { Greet } from "./Greet"
-
-test('Greet renders correctly',()=>{
-    render(<Greet/>)
+describe("Greet", () => {
+  test("Renders correctly", () => {
+    render(<Greet />);
     const textElement = screen.getByText(/hello/i);
     expect(textElement).toBeInTheDocument();
-})
+  });
+});
 
 
-test('Greet renders with the name',()=>{
-    render(<Greet name="Ajay"/>)
-    const textElement = screen.getByText('Hello Ajay');
-    expect(textElement).toBeInTheDocument(); 
-})
+describe('Nested',()=>{
+    test("Renders the name", () => {
+        render(<Greet name="Ajay" />);
+        const textElement = screen.getByText("Hello Ajay");
+        expect(textElement).toBeInTheDocument();
+      });
+  })
